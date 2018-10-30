9a32ba9198f343397e5827fd66607f0da4f49df0
Shaundra <s.m.crittenden@gmail.com>
2018-10-30 02:24:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,4 +92,16 @@ def winner(board)
 end
 
 def play(board)
+  turn(board)
+  over?(board)
 end
+
+# until the game is over
+#   take turns
+# end
+#
+# if the game was won
+#   congratulate the winner
+# else if the game was a draw
+#   tell the players it has been a draw
+# end
